9e7755c175f4981a3d3024fcde9593aa1dca2a5a
Frans Heeremans <mail@fransik.nl>
2018-09-04 16:03:26 +0100

    Backend: Serve the frontend

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const path = require('path');
 
 const apiRoutes = require('./router');
-const { errorHandler } = require('./error');
 
 const app = express();
+const buildDir = path.join(__dirname, '..', 'build');
 
 app.set('port', process.env.PORT || 3000);
 app.use('/api', apiRoutes);
-app.use(errorHandler);
+
+app.use(express.static(buildDir));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(buildDir, 'index.html'));
+});
 
 module.exports = app;
--- backend/router.js
+++ backend/router.js
@@ -1,5 +1,6 @@
 const express = require('express');
 
+const { errorHandler } = require('./error');
 const authProvider = require('./auth');
 const authController = require('./auth/auth.controller');
 const userController = require('./users/user.controller');
@@ -14,5 +15,6 @@ router.use('/auth', authController);
 router.use('/users', userController);
 router.use('/tasks', taskController);
 router.use('/rotas', rotaController);
+router.use(errorHandler);
 
 module.exports = router;

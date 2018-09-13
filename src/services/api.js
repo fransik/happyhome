4bbdb8a4ba0e3b53416304c60f59e2c1480c55dc
Frans Heeremans <mail@fransik.nl>
2018-09-13 16:41:16 +0100

    Frontend: Add env options to change API location

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,13 @@
 import axios from 'axios';
 
-const client = axios.create({
-  baseURL: 'http://localhost:3000/api'
-});
+const client = createClient();
+
+function createClient() {
+  const proto = process.env.REACT_APP_PROTO || 'http';
+  const domain = process.env.REACT_APP_DOMAIN || 'localhost:3000';
+
+  return axios.create({ baseURL: `${proto}://${domain}/api` });
+}
 
 export function setAuthHeader(token) {
   const header = 'Bearer ' + token;

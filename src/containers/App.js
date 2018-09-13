acbaaffd08cc2c953fc4f8681e325f178fc9e134
Frans Heeremans <mail@fransik.nl>
2018-09-13 16:52:00 +0100

    Frontend: Check if user is already authenticated on app boot

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
+import { checkAuth } from '../services/auth';
 import PrivateRoute from '../hoc/PrivateRoute';
 import Header from '../components/Header';
 import { Container } from '../components/Base';
@@ -18,6 +19,7 @@ export default class App extends Component {
 
   state = {
     ...defaultContext,
+    auth: checkAuth(),
     toggleAuth: this.toggleAuth
   };
 
